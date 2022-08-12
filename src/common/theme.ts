2ebe275b71ebae561d8bc5f78bbe94f8388eecb5
oleg.gavashi@yahoo.com <oleg.gavashi@yahoo.com>
2022-08-12 21:37:38 +0300

    1 - Add mui theme

--- src/common/theme.ts
+++ src/common/theme.ts
@@ -1,15 +1,63 @@
 import { createTheme } from '@mui/material';
 
+declare module '@mui/material/styles' {
+  interface Palette {
+    backgroundGrey: string;
+  }
+  interface PaletteOptions {
+    backgroundGrey: string;
+  }
+}
+
+const theme = createTheme({
+  spacing: 10,
+});
+
 const defaultTheme = createTheme({
   palette: {
     primary: {
-      main: '#F50057',
+      main: '#282828',
+      contrastText: '#00B589',
     },
+    secondary: {
+      main: '#00B589',
+      contrastText: '#fff',
+    },
+    background: {
+      default: '#1A1A1A',
+    },
+    text: {
+      primary: '#fff',
+      secondary: '#00B589',
+    },
+
+    backgroundGrey: '#282828',
   },
   typography: {
     h1: {
       fontSize: 40,
       fontWeight: 600,
+    },
+  },
+  shape: {
+    borderRadius: 10,
+  },
+  components: {
+    MuiInputBase: {
+      styleOverrides: {
+        root: {
+          backgroundColor: '#373737',
+          color: '#fff',
+          borderRadius: 10,
+          padding: theme.spacing(),
+          '&.Mui-focused': {
+            border: '1px solid #00B589',
+            '& .MuiOutlinedInput-notchedOutline': {
+              border: 'none',
+            },
+          },
+        },
+      },
     },
   },
 });
