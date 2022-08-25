ab88705608cdef9e0b38efe09d4236d08da9789e
Igor <igormadjar58@gmail.com>
2022-08-25 04:23:18 +0300

    Add handleChange prop to search component

--- src/components/Unknown/Search/index.tsx
+++ src/components/Unknown/Search/index.tsx
@@ -3,16 +3,21 @@ import { InputAdornment, TextField } from '@mui/material';
 import { SearchIcon } from '../Icons';
 import useStyles from './styles';
 
-const Search: React.FC = () => {
+type SearchProps = {
+  handleChange: () => void;
+};
+
+const Search: React.FC<SearchProps> = ({ handleChange }) => {
   const styles = useStyles();
 
   return (
     <TextField
-      className={styles.searchField}
-      placeholder="Search or type"
-      size="small"
-      fullWidth
       variant="standard"
+      size="small"
+      placeholder="Search or type"
+      fullWidth
+      className={styles.searchField}
+      onChange={handleChange}
       InputProps={{
         startAdornment: (
           <InputAdornment position="start">
